1ba534dfd53b556632e8fd4574646dba536d5bca
Jason Gaylord <jason@jasongaylord.com>
2025-07-15 15:30:09 -0400

    More Updates

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ window.addEventListener('DOMContentLoaded', function() {
     const leaderboardTag = document.getElementById('leaderboard-tag');
     if (leaderboardTag) {
         const words = leaderboardTag.textContent.split(' ');
-        if (words.length > 1) {
-            words[1] = `<span class="highlight">${words[1]}</span>`;
+        if (words.length > 2) {
+            words[2] = `<span class="highlight">${words[2]}</span>`;
             leaderboardTag.innerHTML = words.join(' ');
         }
     }
